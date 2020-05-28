faec72427141e4d141321711fed51b0b9745f558
Crowdbotics <team@crowdbotics.com>
2020-05-28 13:58:52 +0000

    Initial commit.

---
+++ backend/evie_3d_house_desig_17506/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for evie_3d_house_desig_17506 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evie_3d_house_desig_17506.settings')
+
+application = get_wsgi_application()
